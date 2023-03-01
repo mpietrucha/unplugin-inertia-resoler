a5b77d8173b1b71de484c07da9e876c2ed500c2e
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-03-01 13:03:31 +0100

    wip

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = function InertiaResolver(config = { except: [] }) {
+const from = '@inertiajs/vue3'
+
+const components = (config = { except: [] }) => {
     const components = ['Head', 'Link'].filter(component => ! config.except.includes(component))
 
     return name => {
@@ -6,6 +8,20 @@ module.exports = function InertiaResolver(config = { except: [] }) {
             return
         }
 
-        return { name, from: '@inertiajs/vue3' }
+        return { name, from }
     }
 }
+
+const composables = (config = { except: [] }) => {
+    const composables = ['useForm'].filter(component => ! config.except.includes(component))
+
+    return name => {
+        if (! composables.includes(name)) {
+            return
+        }
+
+        return { name, from }
+    }
+}
+
+module.exports = { components, composables }
